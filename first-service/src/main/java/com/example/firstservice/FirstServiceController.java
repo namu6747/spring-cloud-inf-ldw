7bdc2bcecddae42167b5da0a255b2f875e15ee69
namu6747 <namu6747@naver.com>
2023-05-06 00:41:51 +0900

    filter & eureka link with gateway

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,14 +1,46 @@
 package com.example.firstservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+    private final Environment env;
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First Service";
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        return header;
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        String[] activeProfiles = env.getActiveProfiles();
+        for (String activeProfile : activeProfiles) {
+            log.warn("activeProfile : {}", activeProfile);
+        }
+
+        String[] defaultProfiles = env.getDefaultProfiles();
+        for (String defaultProfile : defaultProfiles) {
+            log.error("defaultProfile: {}", defaultProfile);
+        }
+        return String.format("hi, message from first on PORT %s", env.getProperty("local.server.port"));
+    }
+
 }
