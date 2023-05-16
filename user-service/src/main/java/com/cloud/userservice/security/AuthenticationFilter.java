7049ededbe1b117681c4db262a6feab5533f45a5
namu6747 <namu6747@naver.com>
2023-05-16 22:53:56 +0900

    add authorization config

---
+++ user-service/src/main/java/com/cloud/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.cloud.userservice.security;
+
+import com.cloud.userservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.annotations.Filter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            RequestLogin requestLogin = objectMapper.readValue(request.getInputStream(), RequestLogin.class);
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestLogin.getEmail(),
+                            requestLogin.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+    }
+}
--- user-service/src/main/java/com/cloud/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/com/cloud/userservice/security/SecurityConfig.java
@@ -1,27 +1,50 @@
 package com.cloud.userservice.security;
 
+import com.cloud.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.util.ConditionalOnBootstrapEnabled;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final Environment environment;
 
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
-        log.error("filter chain loaded");
         http.csrf().disable();
         http.authorizeHttpRequests(auth ->
-                auth.anyRequest().permitAll());
+                auth.requestMatchers("/**")
+                    .access(new WebExpressionAuthorizationManager("hasIpAddress('192.168.0.8')"))
+
+        );
+        http.authorizeRequests(auth ->
+                auth.requestMatchers("/**")
+                        .hasIpAddress("192.168.0.8")
+        );
+        http.addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
         return http.build();
     }
+
+    private AuthenticationFilter getAuthenticationFilter() {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+//        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
+    }
+
 
     @Bean
     PasswordEncoder passwordEncoder() {
