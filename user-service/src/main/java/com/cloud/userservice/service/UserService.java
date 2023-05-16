4d754543e5c9b45f38be3bfea7b269f65f23b1cf
namu6747 <namu6747@naver.com>
2023-05-16 22:53:10 +0900

    add userDetailsService

--- user-service/src/main/java/com/cloud/userservice/service/UserService.java
+++ user-service/src/main/java/com/cloud/userservice/service/UserService.java
@@ -2,8 +2,9 @@ package com.cloud.userservice.service;
 
 import com.cloud.userservice.dto.UserDto;
 import com.cloud.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
--- user-service/src/main/java/com/cloud/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/cloud/userservice/service/UserServiceImpl.java
@@ -10,6 +10,10 @@ import org.hibernate.exception.ConstraintViolationException;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -70,5 +74,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public Iterable<UserEntity> getUserByAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+        return User.withUsername(userEntity.getEmail()).password(userEntity.getEncryptedPwd()).authorities(AuthorityUtils.NO_AUTHORITIES).build();
     }
 }
